9c546b8c604fa7480f8c4c523eecb966c6733694
Joseph Morrison <josephmorrison@postcodelottery.co.uk>
2023-01-22 18:57:11 +0000

    chore: first cdk site commit

--- bin/sitelauncher.ts
+++ bin/sitelauncher.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { SitelauncherStack } from '../lib/sitelauncher-stack';
+import * as cdk from '@aws-cdk/core';
+import SiteLauncherStack from '../lib/sitelauncher-stack';
 
 const app = new cdk.App();
-new SitelauncherStack(app, 'SitelauncherStack', {
+const stackName = 'SitelauncherStack-' + process.env.STAGE;
+new SiteLauncherStack(app, stackName, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
